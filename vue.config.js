7db11957e106701da32287d26a29fd02df51ceb0
Mr-Wang-001 <1269354118@qq.com>
2021-01-07 15:39:48 +0800

    webpack分包

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+
+const path = require('path');
+const webpack = require('webpack')
+const isProduction = process.env.NODE_ENV === 'production'
+
 const CompressionPlugin = require('compression-webpack-plugin')
 const productionGzipExtensions = ['js', 'css']
 const Timestamp = new Date().getTime();
@@ -17,6 +22,10 @@ module.exports = {
         test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
         threshold: 10240,
         minRatio: 0.8
+      }),
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 5, 
+        minChunkSize: 100
       })
     ],
     externals: {
